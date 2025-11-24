a7e1da71a01e08873a5e7b868ef3fed50af828e6
João J <etiotndn@gmail.com>
2025-11-24 15:17:38 -0300

    configura para o ícone aparecer apenas quando o checkbox está ativo

--- src/features/draw-symbols/node.tsx
+++ src/features/draw-symbols/node.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { NodeProps, Position } from "@xyflow/react";
 import DrawSymbolsIcon from "@/assets/icons/svg/draw-symbols.svg?react"
+import RecycleIcon from "@/assets/icons/svg/recycle.svg?react"
 import { IDrawSymbolsNode } from "@/config/types";
 import { BaseNode } from "@/components/ui/base-node";
 import { NodeHandle } from "@/components/ui/node-handle";
@@ -10,9 +11,35 @@ type IProps = NodeProps<IDrawSymbolsNode>;
 
 export const DrawSymbolsNode: React.ComponentType<IProps> = ({ data, isConnectable, selected, id }) => {
   return (
-    <BaseNode id={id} selected={selected} name={data.name} label={data.drawAmount} status={data.status} icon={<DrawSymbolsIcon className="w-14 h-14 mb-3" />}>
-      <NodeHandle id={"draw-target-" + id} type="target" dataType={data.inputType} position={Position.Left} isConnectable={isConnectable} />
-      <NodeHandle id={"draw-source-" + id} type="source" dataType={data.outputType} position={Position.Right} isConnectable={isConnectable} />
-    </BaseNode>
+      <BaseNode
+          id={id}
+          selected={selected}
+          name={data.name}
+          label={data.drawAmount}
+          status={data.status}
+          icon={
+              <>
+                  <DrawSymbolsIcon className="w-14 h-14 mb-3" />
+                  {data.replacement && (
+                      <RecycleIcon className="w-5 h-5 mt-3 mr-2" />
+                  )}
+              </>
+          }
+      >
+          <NodeHandle
+              id={"draw-target-" + id}
+              type="target"
+              dataType={data.inputType}
+              position={Position.Left}
+              isConnectable={isConnectable}
+          />
+          <NodeHandle
+              id={"draw-source-" + id}
+              type="source"
+              dataType={data.outputType}
+              position={Position.Right}
+              isConnectable={isConnectable}
+          />
+      </BaseNode>
   );
 };
